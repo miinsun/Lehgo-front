beec0d59be4386825a0475bfe12432b692188776
JisuKong <kongjisu52@gmail.com>
2022-02-22 19:26:45 +0900

    지도 마커 방식 수정

--- src/components/map/Map.vue
+++ src/components/map/Map.vue
@@ -1,19 +1,9 @@
 <template>
 <transition name="map">
   <div v-if="loaded">
-    <naver-maps
-      :height="height"
-      :width="width"
-      :mapOptions="mapOptions"
-      :initLayers="initLayers"
-      @load="onLoad"
-      @idle="checkMacker">
-        <naver-info-window
-          class="info-window"
-          @load="onWindowLoad"
-          :isOpen="info"
-          :marker="selectedMarker"
-          :moreOptions="windowOptions">
+    <naver-maps :height="height" :width="width" :mapOptions="mapOptions" :initLayers="initLayers" @load="onLoad" @idle="checkMacker">
+        <div v-if="getLoaded">
+        <naver-info-window class="info-window" @load="onWindowLoad" :isOpen="info" :marker="selectedMarker" :moreOptions="windowOptions">
           <draggable class="list-group mainCourse"  v-if="info" :list="[{place : selectedPlace}]"
             :group="{name : 'mainCourse', pull: 'clone', put: false }">
             <v-btn text class="list-group-item placeTitleBtn" elevation="4">
@@ -22,13 +12,16 @@
           </draggable>
         </naver-info-window>
         <naver-marker v-for="p, i in placeList" :key="'p' + i"
-          :lat="p.place.latitude" :lng="p.place.longitude" @click="onMarkerClicked(p.place, $event)" @load="onMarkerLoaded(p.place, $event)"/>
-        <naver-marker v-for="p, i  in coursePlaceList" :key="'c' + i"
-          :lat="p.place.latitude" :lng="p.place.longitude" @click="onMarkerClicked(p.place, $event)" @load="onCourseMarkerLoaded(p.place, $event)"/>
+          :lat="p.place.latitude" :lng="p.place.longitude" @click="onMarkerClicked(p.place)" @load="onMarkerLoaded(p.place, $event)"/>
+        <naver-marker v-for="c, i  in coursePlaceList" :key="'c' + i"
+          :lat="c.place.latitude" :lng="c.place.longitude" @click="onMarkerClicked(c.place)" @load="onCourseMarkerLoaded(c.place, $event)"/>
+        <naver-marker 
+          :lat="0"  :lng="0" @click="cancelMarkerClicked()" @load="onSelectedMarkerLoaded"/>
         <div v-for="p, i in coursePlaceList" :key="'line' + i">
         <naver-polyline  v-if="i != 0 && reLoad"
         :path="[{lat: coursePlaceList[i - 1].place.latitude, lng:coursePlaceList[i - 1].place.longitude},
             {lat: coursePlaceList[i].place.latitude, lng:coursePlaceList[i].place.longitude}]"/>
+        </div>
         </div>
     </naver-maps>
   </div>
@@ -102,36 +95,30 @@ import courseMarker from '@/assets/marker-course.png'
           }
         }
       },
-      onMarkerClicked(place, event){
-        let idx = event.overlay.title;
-        this.onPlaceClicked(place, idx);
+      cancelMarkerClicked(){
+        this.info = false;
+        this.setPlace(null);
+        this.selectedMarker.setVisible(false);
       },
-      onPlaceClicked(place, idx){
-        if(this.info){
-          this.selectedMarker.setIcon(this.originIcon)
-          this.selectedMarker.setZIndex(0)
-          this.setPlace(null)
-          this.info = false
+      onMarkerClicked(place){
+        if(this.selectedPlace == place){
+          this.changeSelected(place);
         }
-        else if(this.selectedPlace == place){
-          this.changeSelected(idx, place);
-        }
-        if(this.selectedPlace != place){
+        else{
+          this.info = false;
           this.map.morph({y : place.latitude, x: place.longitude})
-          setTimeout(() => this.changeSelected(idx, place), 500);
+          setTimeout(() => this.changeSelected(place), 500);
         }
       },
-      changeSelected(idx, place){
+      changeSelected(place){
         let length, i, c;
         for ((length=i=0); (c=place.placeName.charCodeAt(i++));(length+=c>>11?3:c>>7?2:1));
         this.windowOptions.pixelOffset.x = (length < 15) ? -50 : length * (-3.3)
         this.setPlace(place)
-        this.originIcon = this.marker[idx].getIcon()
-        this.selectedMarker = this.marker[idx]
-        this.marker[idx].setIcon(selectMarkerIcon)
-        this.marker[idx].setZIndex(1)
-        this.selectedPlace = place
-        this.info = true
+        this.selectedPlace = place;
+        this.selectedMarker.setPosition({y : place.latitude, x: place.longitude});
+        this.selectedMarker.setVisible(true);
+        this.info = true;
       },
       onWindowLoad(){
       },
@@ -139,32 +126,46 @@ import courseMarker from '@/assets/marker-course.png'
         let newMarker = event.marker;
         newMarker.setTitle(this.marker.length);
         this.placeMarkerList[place.placeId] = this.marker.length
-        this.marker.push(newMarker);
+        this.marker.push(event.marker);
       },
       onCourseMarkerLoaded(place, event) {
         let newMarker = event.marker;
         newMarker.setIcon(courseMarker)
         newMarker.setTitle(this.marker.length);
+        newMarker.setZIndex(10);
         this.placeMarkerList[place.placeId] = this.marker.length
         this.marker.push(newMarker);  
+      },
+      onSelectedMarkerLoaded(event){
+        let newMarker = event.marker;
+        newMarker.setIcon(selectMarkerIcon);
+        newMarker.setZIndex(20);
+        newMarker.setVisible(false);
+        this.selectedMarker = newMarker;
       },
       reRoadPath(){
         this.reLoad = false
         setTimeout(() =>(this.reLoad = true), 1);
       },
       setMapPlace(){
+        console.log(this.getPlaceList.length, this.getCoursePlaceList.length)
         this.marker = [];
         this.loaded = false;
-        setInterval(() => this.loaded = true, 100);
-        if(this.coursePlaceList.length > 0){
-            this.selectedPlace = this.coursePlaceList[0].place
+        if(this.getCoursePlaceList.length && this.getCoursePlaceList.length > 0 && this.getPlaceList.length > 0){
+            this.selectedPlace = this.getCoursePlaceList[0].place
             this.mapOptions.lat = this.selectedPlace.latitude;
             this.mapOptions.lng = this.selectedPlace.longitude;
-            this.placeList = courseService.notCoursePlaceList(this.getPlaceList, this.coursePlaceList);
+            this.coursePlaceList = this.getCoursePlaceList;
+            this.placeList = courseService.notCoursePlaceList(this.getPlaceList, this.getCoursePlaceList);
+        }
+        else if (this.getPlaceList == null){
+          this.placeList = [];
         }
         else{
+            this.selectedPlace =  this.getPlaceList[0]
             this.placeList = this.getPlaceList
         }
+        setInterval(() => this.loaded = true, 100);
       },
       ...placeMapActions(['setPlace'])
     },
@@ -173,22 +174,27 @@ import courseMarker from '@/assets/marker-course.png'
         this.width = window.innerWidth * this.mapCol;
     },
     computed: {
-        ...listMapGetters(['getPlaceList'])
+        ...listMapGetters(['getLoaded', 'getPlaceList', 'getCoursePlaceList'])
     },
     watch:{
-        coursePlaceList: function(){
+        getCoursePlaceList: function(){
+          this.coursePlaceList = this.getCoursePlaceList;
+          this.placeList = courseService.notCoursePlaceList(this.getPlaceList, this.getCoursePlaceList);
           this.reRoadPath()
-          this.placeList = courseService.notCoursePlaceList(this.getPlaceList, this.coursePlaceList);
+        },
+        getPlaceList:function(){
+            this.coursePlaceList = this.getCoursePlaceList;
+            this.placeList = courseService.notCoursePlaceList(this.getPlaceList, this.getCoursePlaceList);
         },
         clickedPlace: function(){
-          this.onPlaceClicked(this.clickedPlace, this.placeMarkerList[this.clickedPlace.placeId]);
+          this.onMarkerClicked(this.clickedPlace);
         },
         mapCol : function(){
           this.map.setSize({width: window.innerWidth * this.mapCol, height: this.height})
-        }
+        },
     },
     created() {
-        this.setMapPlace();
+      this.setMapPlace();
     }
   }
 </script>
@@ -203,5 +209,8 @@ import courseMarker from '@/assets/marker-course.png'
   border: none;
   color: #000;
   background-color: white;
+}
+.courseMarker{
+  z-index: 10;
 }
 </style>
