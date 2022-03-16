2b33ada9e61bf0d3d41b1c1297db59025b6d2bac
민선 <61786235+miinsun@users.noreply.github.com>
2022-03-17 03:05:31 +0900

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     //SpringBoot 서버 접속 주소
     devServer: {
-        proxy: "http://localhost:8080"
+        proxy: "http://localhost:9999"
     },
 
     chainWebpack: config => {  
